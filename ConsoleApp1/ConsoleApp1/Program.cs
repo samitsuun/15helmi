4d048a770e6b5dbb9a4ba82bd11c5ad508e48edd
Sami Sokura <sami1710@student.hamk.fi>
2018-02-15 15:35:41 +0200

    tiedoston muokkaus kokeilu

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> rivit = new List<string>();
+
+            StreamReader reader = File.OpenText(@"C:\repositories\myfile.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                rivit.Add(line);
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+
+            //TODO: muokkaa
+            for (int i = 0; i < rivit.Count; i++)
+            {
+                rivit[i]=rivit[i].ToUpper(); //muuttaa kirjaimet isoiksi
+            }
+
+
+                StreamWriter writer = File.CreateText(@"C:\repositories\myfile.txt");
+            for (int i = 0; i < rivit.Count; i++)
+            {
+                writer.WriteLine(rivit[i]);
+            }
+            writer.Close();
+            
+        }
+    }
+}
