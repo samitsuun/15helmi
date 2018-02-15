5b241488c6c26370b3824360cf5eefb65fb0901c
Sami Sokura <sami1710@student.hamk.fi>
2018-02-15 15:36:59 +0200

    laskin

---
+++ laskin/laskin/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laskin
+{
+    public override class YhteenLaskin : Laskin
+    {
+        public int yhteenlasku(int Luku1, int Luku2) : base(Laske)
+        {
+            return Luku1 + Luku2;
+        }
+    }
+
+    public class VähennysLaskin : Laskin
+    {
+        public int vähennyslasku(int Luku1, int Luku2) : base(Laske)
+        { 
+            return Luku1 - Luku2;
+        }
+    }
+
+    public class JakoLaskin : Laskin
+    {
+        public int jakolasku(int Luku1, int Luku2) : base(Laske)
+        {
+            return Luku1 / Luku2;
+        }
+    }
+    public class KertoLaskin : Laskin
+    {
+        public int Kertolasku(int Luku1, int Luku2) : base(Laske)
+        {
+            return Luku1 * Luku2;
+        }
+    }
+}
---
+++ laskin/laskin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laskin
+{
+    public abstract class Laskin
+    {
+        public abstract int Laske(int Luku1, int Luku2);
+    }
+}
